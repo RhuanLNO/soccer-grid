ce2251fa12267ae072406594f02a5b790f6d7390
JandersonCRB <jandersonangelo@hotmail.com>
2024-07-19 19:49:26 -0300

    creating api hook and fetching clubs from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import SquareGrid from "@/components/SquareGrid";
 import { Trophy } from "lucide-react";
 import Link from "next/link";
-import {
-  Dialog,
-} from "@/components/ui/dialog"
+import { Dialog } from "@/components/ui/dialog";
 import TeamSelectModal from "@/components/TeamSelectModal";
+import { useApi } from "@/hooks/apiHook";
 
 export default function Home() {
+  const { api } = useApi();
+
+  console.log(api);
   return (
     <main className="flex min-h-screen flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b border-slate-200 bg-background px-4 md:px-6 justify-center">
@@ -25,24 +29,36 @@ export default function Home() {
         <div className="flex flex-col md:mr-32 place-items-center">
           <div className="flex flex-row">
             <div className="w-24 md:w-40 h-20 text-center content-end pb-4" />
-            <div className="w-24 md:w-40 h-20 text-center content-end pb-4">Tip</div>
-            <div className="w-24 md:w-40 h-20 text-center content-end pb-4">Tip</div>
-            <div className="w-24 md:w-40 h-20 text-center content-end pb-4">Tip</div>
+            <div className="w-24 md:w-40 h-20 text-center content-end pb-4">
+              Tip
+            </div>
+            <div className="w-24 md:w-40 h-20 text-center content-end pb-4">
+              Tip
+            </div>
+            <div className="w-24 md:w-40 h-20 text-center content-end pb-4">
+              Tip
+            </div>
           </div>
           <div className="flex flex-row">
             <div className="flex flex-col">
-              <div className="size-24 md:size-40 text-center content-center">Tip</div>
-              <div className="size-24 md:size-40 text-center content-center">Tip</div>
-              <div className="size-24 md:size-40 text-center content-center">Tip</div>
+              <div className="size-24 md:size-40 text-center content-center">
+                Tip
+              </div>
+              <div className="size-24 md:size-40 text-center content-center">
+                Tip
+              </div>
+              <div className="size-24 md:size-40 text-center content-center">
+                Tip
+              </div>
             </div>
             <div className="border border-slate-200">
               <div className="flex flex-row border-b border-slate-200">
-                <SquareGrid img='https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg' />
+                <SquareGrid img="https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg" />
                 <SquareGrid />
                 <SquareGrid />
               </div>
               <div className="flex flex-row border-b border-slate-200">
-                <SquareGrid img='https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png' />
+                <SquareGrid img="https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png" />
                 <SquareGrid />
                 <SquareGrid />
               </div>
---
+++ src/hooks/clubsHook.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { useApi } from "@/hooks/apiHook";
+import { AxiosError } from "axios";
+
+type Club = {
+  clubKey: string;
+  name: string;
+};
+
+type SnakeCaseClub = {
+  club_key: string;
+  name: string;
+};
+
+type ClubsHook = {
+  clubs: Club[];
+  isLoading: boolean;
+  error: string | null;
+  hasError: boolean;
+};
+
+type ClubsProviderProps = {
+  children: ReactNode;
+};
+
+const mapClubFields = (snakeCaseClub: SnakeCaseClub): Club => {
+  return {
+    clubKey: snakeCaseClub["club_key"],
+    name: snakeCaseClub["name"],
+  };
+};
+
+const ClubsContext = createContext<ClubsHook | null>(null);
+
+const ClubsProvider = ({ children }: ClubsProviderProps) => {
+  const { api } = useApi();
+  const [clubs, setClubs] = useState<Club[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const hasError = useMemo(() => error !== null, [error]);
+
+  const fetchClubs = useCallback(async () => {
+    try {
+      const response = await api.get<SnakeCaseClub[]>("api/v1/clubs");
+
+      setClubs(response.data.map(mapClubFields));
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data?.message ?? "An error occurred");
+        console.error(error);
+      } else {
+        throw error;
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }, [api]);
+
+  useEffect(() => {
+    void fetchClubs();
+  }, [fetchClubs]);
+
+  return (
+    <ClubsContext.Provider value={{ clubs, isLoading, error, hasError }}>
+      {children}
+    </ClubsContext.Provider>
+  );
+};
+
+const useClubs = () => {
+  const clubsHook = useContext(ClubsContext);
+  if (!clubsHook) {
+    throw new Error("useClubs must be called within a ClubsProvider context");
+  }
+};
+
+export { ClubsProvider, useClubs };
