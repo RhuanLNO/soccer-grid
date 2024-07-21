75ab32c492977c31aa55d8f13e557cac4991bd5a
Rhuan Levy <rhuan_os@hotmail.com>
2024-07-21 19:35:23 -0300

    refactor: call todayGrid fetch with useApi hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ import {
   Dialog,
 } from "@/components/ui/dialog";
 import TeamSelectModal from "@/components/TeamSelectModal";
-import { useState, useEffect } from "react";
-import { getTodayGrid } from "@/requests/requests";
+import { useState, useEffect, useCallback } from "react";
+import { useApi } from "@/hooks/apiHook";
+import { AxiosError } from "axios";
 
 export type GridTips = {
   id: number,
@@ -28,23 +29,31 @@ export type PuzzleGrid = {
 
 export default function Home() {
   const [todayGrid, setTodayGrid] = useState<PuzzleGrid>();
-  const [loading, setLoading] = useState<Boolean>(true);
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  const fetchTodayGrid = async () => {
+  const { api } = useApi();
+
+  const fetchTodayGrid = useCallback(async () => {
     try {
-      getTodayGrid().then((res) => {
-        setTodayGrid(res.data);
-        setLoading(false);
-      })
+      const response = await api.get<PuzzleGrid>("api/v1/grids/today");
+
+      setTodayGrid(response.data);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data?.message ?? "An error occurred");
+        console.error(error);
+      } else {
+        throw error;
+      }
+    } finally {
+      setIsLoading(false);
     }
-    catch (err) {
-      console.error(err)
-    }
-  };
+  }, [api]);
 
   useEffect(() => {
     fetchTodayGrid();
-  }, []);
+  }, [fetchTodayGrid]);
 
   return (
     <main className="flex min-h-screen flex-col">
@@ -62,7 +71,7 @@ export default function Home() {
       </header>
       <Dialog>
         <div className="flex flex-col md:mr-32 place-items-center mt-16">
-          {loading &&
+          {isLoading &&
             <div role="status">
               <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
