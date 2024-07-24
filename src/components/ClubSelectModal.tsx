bf437d10ff76eeb287aeb27424b07bb36c0e529c
Janderson <janderson@quimbik.com>
2024-07-23 23:35:46 -0300

    Running prettier on ClubSelectModal.tsx

--- src/components/ClubSelectModal.tsx
+++ src/components/ClubSelectModal.tsx
@@ -1,29 +1,32 @@
-"use client";
+'use client';
 
-import { useMemo, useState } from "react";
+import { useMemo, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Command, CommandInput, CommandList } from '@/components/ui/command';
 import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
-import { Command, CommandInput, CommandList } from "@/components/ui/command";
-import { Button } from "@/components/ui/button";
-import { useClubs } from "@/hooks/clubsHook";
-import { Club } from "@/types/clubTypes";
-import { clubSearchCompare } from "@/lib/clubUtils";
+} from '@/components/ui/dialog';
+import { useClubs } from '@/hooks/clubsHook';
+import { clubSearchCompare } from '@/lib/clubUtils';
+import { Club } from '@/types/clubTypes';
 
 const ClubSelectModal = () => {
   const { clubs } = useClubs();
 
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   const filteredList = useMemo(() => {
     if (value.length < 3) {
       return [];
     }
 
-    return clubs?.filter((club: Club) => {
-      return clubSearchCompare(club, value);
-    }) ?? [];
+    return (
+      clubs?.filter((club: Club) => {
+        return clubSearchCompare(club, value);
+      }) ?? []
+    );
   }, [value, clubs]);
   return (
     <DialogContent>
@@ -36,16 +39,15 @@ const ClubSelectModal = () => {
             onValueChange={(e) => setValue(e)}
           />
           <CommandList>
-            {
-              filteredList.map((team, index) => (
-                <div
-                  key={index}
-                  className="flex justify-between py-1.5 px-2 items-center"
-                >
-                  <span>{team.name}</span>
-                  <Button onClick={() => setValue(team.name)}>Guess</Button>
-                </div>
-              ))}
+            {filteredList.map((team, index) => (
+              <div
+                key={index}
+                className="flex justify-between py-1.5 px-2 items-center"
+              >
+                <span>{team.name}</span>
+                <Button onClick={() => setValue(team.name)}>Guess</Button>
+              </div>
+            ))}
           </CommandList>
         </Command>
       </DialogHeader>
